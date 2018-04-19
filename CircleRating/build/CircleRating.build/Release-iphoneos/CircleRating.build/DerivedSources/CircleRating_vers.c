7d88cf514d7449a82260e64f8aadaf7c0e9a7351
Pradeep Rajkumar <>
2018-04-19 12:54:48 +0530

    Compiled the project with "Release" mode configuration.

---
+++ CircleRating/build/CircleRating.build/Release-iphoneos/CircleRating.build/DerivedSources/CircleRating_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char CircleRatingVersionString[];
+ extern const double CircleRatingVersionNumber;
+
+ const unsigned char CircleRatingVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CircleRating  PROJECT:CircleRating-1" "\n";
+ const double CircleRatingVersionNumber __attribute__ ((used)) = (double)1.;
