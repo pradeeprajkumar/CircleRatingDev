f20512a114819c87fb73189e965319188019e85a
Pradeep Rajkumar <rajkumar.pradeep@yahoo.com>
2018-04-23 12:59:31 +0530

    Framework build related changes.

---
+++ CircleRating/build/CircleRating.build/Debug-iphonesimulator/CircleRating.build/Objects-normal/i386/CircleRating_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/pradeeprajkumar/Work/Projects/iOS/Github/CircleRatingDev/CircleRating/build/CircleRating.build/Debug-iphonesimulator/CircleRating.build/DerivedSources/CircleRating_vers.c
